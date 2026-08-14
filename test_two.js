df8c828569bfabdad7777881da6582b8ee7d8877
McQueen010 <favourmacqueen8@gmail.com>
2026-08-14 11:24:23 +0100

    test two

---
+++ test_two.js
@@ -0,0 +1,9 @@
+
+let User_name = "admin";
+let password = 123456
+
+if (!User_name && !password) {
+    console.log("Invalid Username or Password")
+} else {
+    console.log("Login Successful")
+}
